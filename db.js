95401efa9d6a31bb29e67de23772b97195571c8e
RuizSP <84715726+RuizSP@users.noreply.github.com>
2024-05-20 10:48:57 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Configuração da conexão com o banco de dados
 const pool = new Pool({
-    user: 'postgres',
+    user: 'seuusuarioaqui',
     host: '127.0.0.1',
-    database: 'postgres',
-    password: '1234',
+    database: 'seudatabaseaqui',
+    password: 'suasenhaqui',
     port: 5432, // Porta padrão do PostgreSQL
 });
 
